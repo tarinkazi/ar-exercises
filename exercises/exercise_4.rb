579e41a69bd1779d7f8ddf9d9129b16c537839ac
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2022-01-05 02:20:14 -0500

    exe 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,13 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+# puts Store.count
+
+@mens_stores = Store.where(mens_apparel: true)
+
+Store.select(:name, :annual_revenue)
